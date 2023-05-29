6bc8978fcbe8cf43a9ffaa7bb384113c628b19a2
Zero <54469706+3JoB@users.noreply.github.com>
2023-05-29 20:38:12 +0800

    add

--- build.zig
+++ build.zig
@@ -8,10 +8,4 @@ pub fn build(b: *std.build.Builder) void {
     const lib = b.addStaticLibrary("anthropic-sdk-zig", "src/main.zig");
     lib.setBuildMode(mode);
     lib.install();
-
-    const main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
 }
