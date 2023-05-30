6a109ccacb972a671d569959291e3bdf195c60fd
Zero <54469706+3JoB@users.noreply.github.com>
2023-05-30 12:00:20 +0800

    add

--- src/main.zig
+++ src/main.zig
@@ -9,6 +9,10 @@ const string = []const u8;
 pub const Client = struct {
     key: string,
     defaultModel: string = "claude-instant-v1.1",
+
+    pub fn send(self: *Client) void {
+        std.debug.print("{s}\n", .{self.defaultModel});
+    }
 };
 
 pub const sender = struct {
@@ -20,15 +24,17 @@ pub const sender = struct {
     top_k: i32 = -1,
     top_p: i32 = -1,
     metadata: MetaData,
+
+    pub fn setUserID(s: *sender, user_id: string) void {
+        s.metadata.user_id = user_id;
+    }
 };
 
 pub const MetaData = struct {
     user_id: ?string = null,
 };
 
-pub fn setUserID(s: *sender, user_id: string) void {
-    s.metadata.user_id = user_id;
-}  
+
 
 const Response = struct {
     cache: string = "",
@@ -39,31 +45,27 @@ const Response = struct {
     exception: ?string = null,
     model: string,
     truncated: bool,
-};  
 
-fn resp_to_string(resp: *Response) string {
-    if (resp.cache.len != 0) {
-        return resp.cache; 
+    pub fn to_string(resp: *Response) string {
+        if (resp.cache.len != 0) {
+            return resp.cache; 
+        }
+        const buf: [100]u8 = undefined;
+        const len = std.fmt.bufPrintZ(&buf, "{}", .{resp}) catch unreachable;
+        resp.cache = buf[0..len];
+        return resp.cache;
     }
-    const buf: [100]u8 = undefined;
-    const len = std.fmt.bufPrintZ(&buf, "{}", .{resp}) catch unreachable;
-    resp.cache = buf[0..len];      
-    return resp.cache;
-}
+}; 
 
 // Creat a New Anthropic Client
 pub fn new(client: Client) Client {
     return client;
 }
 
-pub fn send(c: Client) void {
-    std.debug.print("{s}\n", .{c.defaultModel});
-}
-
 pub fn main() void {
     var cn = Client{
         .key = "12354",
     };
-    send(cn);
+    cn.send();
     std.debug.print("{s}\n", .{cn.key});
 }
